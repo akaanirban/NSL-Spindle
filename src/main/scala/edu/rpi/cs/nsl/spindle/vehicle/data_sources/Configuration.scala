52fc3ff82449a8455b37b613344cf36a112a6674
William Rory Kronmiller <wrk961@gmail.com>
2016-12-24 16:24:41 -0500

    Push updates to fork

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/data_sources/Configuration.scala
+++
@@ -1,7 +0,0 @@
-package edu.rpi.cs.nsl.spindle.vehicle.data_sources
-
-object Configuration {
-  object Kafka {
-    val servers = "localhost:9092"
-  }
-}
--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/data_sources/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Vehicle Sensor data source programs
- * 
- * @author wrkronmiller
- *
- */
-package edu.rpi.cs.nsl.spindle.vehicle.data_sources;
