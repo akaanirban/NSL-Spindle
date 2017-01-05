c776cf513120725052d8bfcb2d867dca70021687
William Rory Kronmiller <wrk961@gmail.com>
2017-01-05 12:33:53 -0500

    Validate docker host

--- src/test/scala/edu/rpi/cs/nsl/spindle/vehicle/Configuration.scala
+++ src/test/scala/edu/rpi/cs/nsl/spindle/vehicle/Configuration.scala
@@ -2,9 +2,16 @@ package edu.rpi.cs.nsl.spindle.vehicle
 
 import org.scalatest.DoNotDiscover
 import scala.sys.process._
+import java.net.InetAddress
 
 @DoNotDiscover
 object Configuration {
   //val hostname = "butterfly-68.dynamic2.rpi.edu"
-  val hostname = "scripts/kafka-docker/get-ip.sh".!!.trim
+  lazy val dockerHost = {
+    val ipAddr = "scripts/kafka-docker/get-ip.sh".!!.trim
+    // Ensure IP is valid
+    InetAddress.getByName(ipAddr).getAddress
+    ipAddr
+  }
+  
 }
