dc6f73fb89c84eea5936925e27b20f9ec62555ed
William Rory Kronmiller <wrk961@gmail.com>
2017-01-10 11:35:43 -0500

    Break test code into multiple files

--- src/test/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/MessageFactorySpec.scala
+++ src/test/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/MessageFactorySpec.scala
@@ -6,29 +6,6 @@ import org.slf4j.LoggerFactory
 import VehicleTypes._
 import edu.rpi.cs.nsl.spindle.datatypes.VehicleColors
 
-object ReflectionFixtures {
-  val mphValue = 100
-  val basicReadingCollection: Iterable[TypedValue[Any]] = Seq(TypedValue[MPH](mphValue), TypedValue[Lat](10), TypedValue[Lon](10)).asInstanceOf[Seq[TypedValue[Any]]]
-  val basicPropertiesCollection: Iterable[TypedValue[Any]] = Seq(TypedValue[VehicleColors.Value](VehicleColors.red), TypedValue[VehicleId](0)).asInstanceOf[Seq[TypedValue[Any]]]
-}
-
-class ReflectionUtilsSpec extends FlatSpec {
-
-  it should "correctly generate a type tag string for vehicle types" in {
-    val expectedString = "TypeTag[edu.rpi.cs.nsl.spindle.datatypes.VehicleTypes.MPH]"
-    assert(ReflectionUtils.getTypeString[MPH] == expectedString)
-    assert(ReflectionUtils.getTypeString[VehicleTypes.MPH] == expectedString)
-    assert(ReflectionUtils.getTypeString[edu.rpi.cs.nsl.spindle.datatypes.VehicleTypes.MPH] == expectedString)
-  }
-
-  it should "correctly isolate objects of a particular type from a collection" in {
-    import ReflectionFixtures.{ basicReadingCollection => collection }
-    val mphResults = ReflectionUtils.getMatchingTypes(collection, ReflectionUtils.getTypeString[MPH])
-    assert(mphResults.size == 1)
-    assert(mphResults.last.value == ReflectionFixtures.mphValue)
-  }
-}
-
 class MessageFactorySpec extends FlatSpec {
   private val logger = LoggerFactory.getLogger(this.getClass)
   private object DummyMessageFactory extends VehicleMessageFactory {
---
+++ src/test/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/ReflectionUtilsSpec.scala
@@ -0,0 +1,22 @@
+package edu.rpi.cs.nsl.spindle.vehicle.simulation
+
+import edu.rpi.cs.nsl.spindle.datatypes._
+import VehicleTypes._
+import org.scalatest.FlatSpec
+
+class ReflectionUtilsSpec extends FlatSpec {
+
+  it should "correctly generate a type tag string for vehicle types" in {
+    val expectedString = "TypeTag[edu.rpi.cs.nsl.spindle.datatypes.VehicleTypes.MPH]"
+    assert(ReflectionUtils.getTypeString[MPH] == expectedString)
+    assert(ReflectionUtils.getTypeString[VehicleTypes.MPH] == expectedString)
+    assert(ReflectionUtils.getTypeString[edu.rpi.cs.nsl.spindle.datatypes.VehicleTypes.MPH] == expectedString)
+  }
+
+  it should "correctly isolate objects of a particular type from a collection" in {
+    import ReflectionFixtures.{ basicReadingCollection => collection }
+    val mphResults = ReflectionUtils.getMatchingTypes(collection, ReflectionUtils.getTypeString[MPH])
+    assert(mphResults.size == 1)
+    assert(mphResults.last.value == ReflectionFixtures.mphValue)
+  }
+}
