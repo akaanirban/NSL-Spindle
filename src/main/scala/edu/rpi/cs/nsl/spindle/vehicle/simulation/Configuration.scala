0c9c12acc055e2124709b25bbde664b6a53ac87f
William Rory Kronmiller <wrk961@gmail.com>
2017-01-13 12:23:20 -0500

    Add sensor configs

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/Configuration.scala
+++ src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/Configuration.scala
@@ -4,6 +4,8 @@ import com.typesafe.config.ConfigFactory
 import com.typesafe.config.Config
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.postgres.PgDefaults
 
+import scala.collection.JavaConverters.asScalaBufferConverter
+
 trait ConfigurationSingleton {
   protected val conf = ConfigFactory.load()
   protected implicit class EnhancedConfig(config: Config) {
@@ -12,6 +14,9 @@ trait ConfigurationSingleton {
         case false => None
         case true  => Some(config.getAnyRef(key).asInstanceOf[T])
       }
+    }
+    def getDoubleList(key: String): List[Double] = {
+      config.getStringList(key).asScala.map(_.toDouble).toList
     }
   }
 }
@@ -32,5 +37,24 @@ object Configuration extends ConfigurationSingleton {
     val database = conf.getOpt[String]("postgres.database").getOrElse(PgDefaults.database)
     val ssl = conf.getOpt[Boolean]("postgres.ssl").getOrElse(PgDefaults.ssl)
     val readOnly = conf.getOpt[Boolean]("postgres.readOnly").getOrElse(PgDefaults.readOnly)
+  }
+  
+  val simStartOffsetMs = 5000
+
+  object Vehicles {
+    object Sensors {
+      private val prefix = "spindle.sim.vehicle.sensors"
+      //TODO: load json, use beans, etc...
+      val singleValSensors = {
+        val singleValNames: List[String] = conf.getStringList(s"$prefix.singlevaluesensor.names").asScala.toList
+        val singleValValues = conf.getDoubleList(s"$prefix.singlevaluesensor.values").asScala.toList
+        singleValNames.zip(singleValValues)
+      }
+      val rngSensors = {
+        val rngValueNames: List[String] = conf.getStringList(s"$prefix.rngvaluesensor.names").asScala.toList
+        val rngValueMaxVals = conf.getDoubleList(s"$prefix.rngvaluesensor.maxvals").asScala.toList
+        rngValueNames.zip(rngValueMaxVals)
+      }
+    }
   }
 }
