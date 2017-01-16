f9ffb0a8a1d417f74bd15403168dc2cb971c56f6
William Rory Kronmiller <wrk961@gmail.com>
2017-01-16 16:11:24 -0500

    Make scripts folder, add script to update submodules

--- scripts/runInContainer.sh
+++ scripts/runInContainer.sh
@@ -2,4 +2,5 @@
 # NOTE: this is used to work around the MacOS hard limit on the number of threads
 # running at a single time. It is also probably not a terrible idea to target
 # Linux instead of MacOS while developing the Vehicle simulator.
+cd ../
 docker run --rm --name spindle-env -v `pwd`:/opt/ -it wkronmiller/scala-sbt
