9b787e9fb0dbb13109fe8fe140779edc8e10df17
William Rory Kronmiller <wrk961@gmail.com>
2017-01-16 15:58:20 -0500

    Update scripts/docs to support linux testing

--- scripts/kafka-docker/env.sh
+++ scripts/kafka-docker/env.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
 export COMPOSE_PROJECT_NAME='NSL-Kafka-Cluster'
-export DOCKER_MACHINE_NAME=nsl
-eval $(docker-machine env $DOCKER_MACHINE_NAME)
-export HOSTNAME=`docker-machine ip $DOCKER_MACHINE_NAME`
+
+if [[ -z "$LOCAL_DOCKER" ]]; then
+    export DOCKER_MACHINE_NAME=nsl
+    eval $(docker-machine env $DOCKER_MACHINE_NAME)
+    export HOSTNAME=`docker-machine ip $DOCKER_MACHINE_NAME`
+fi
