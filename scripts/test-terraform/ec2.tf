9d617047b1a33aee805eb6d9441e20780f8f0daf
William Rory Kronmiller <wrk961@gmail.com>
2017-01-25 12:11:36 -0500

    Expect pre-configured vpc/subnet/security group and hard-code ubuntu xenial docker repo

--- scripts/test-terraform/ec2.tf
+++ scripts/test-terraform/ec2.tf
@@ -11,76 +11,24 @@ variable "instance_type" {
     default = "t2.small"
 }
 
-resource "aws_vpc" "nsl_test_vpc" {
-    cidr_block = "192.168.0.0/24"
-    enable_dns_support = true
-    enable_dns_hostnames = true
+variable "test_vpc" {
+    default = "vpc-fca71899"
 }
 
-resource "aws_subnet" "nsl_test_subnet" {
-    vpc_id = "${aws_vpc.nsl_test_vpc.id}"
-    cidr_block = "192.168.0.0/24"
-    map_public_ip_on_launch = true
+variable "test_security_group" {
+    default = "sg-13b3a46e"
 }
 
-resource "aws_security_group" "nsl_test_allow_ssh" {
-    name = "nsl_test_allow_ssh"
-    vpc_id = "${aws_vpc.nsl_test_vpc.id}"
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+variable "test_subnet" {
+    default = "subnet-12afb03a" 
 }
 
-resource "aws_internet_gateway" "gw" {
-    vpc_id = "${aws_vpc.nsl_test_vpc.id}"
-}
-
-resource "aws_route_table" "rt" {
-    vpc_id = "${aws_vpc.nsl_test_vpc.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.gw.id}"
-    }
-}
-
-resource "aws_route" "internet_route" {
-    route_table_id = "${aws_route_table.rt.id}"
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-}
-
-resource "aws_eip" "nat_ip" {
-    vpc = true
-    depends_on = ["aws_internet_gateway.gw"]
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-    allocation_id = "${aws_eip.nat_ip.id}"
-    subnet_id = "${aws_subnet.nsl_test_subnet.id}"
-    depends_on = ["aws_internet_gateway.gw"]
-}
-
-resource "aws_route_table_association" "internet" {
-    subnet_id = "${aws_subnet.nsl_test_subnet.id}"
-    route_table_id = "${aws_route_table.rt.id}"
-}
-
-/*resource "aws_instance" "sbt_test" {
+resource "aws_instance" "sbt_test" {
     ami =   "${var.kafka-ami}"
-    depends_on = ["aws_internet_gateway.gw"]
     instance_type = "${var.instance_type}"
     associate_public_ip_address = true
-    subnet_id = "${aws_subnet.nsl_test_subnet.id}"
-    vpc_security_group_ids = ["${aws_security_group.nsl_test_allow_ssh.id}"]
+    subnet_id = "${var.test_subnet}"
+    vpc_security_group_ids = ["${var.test_security_group}"]
     key_name = "${var.ssh_key_name}"
     root_block_device {
         volume_size =   120
@@ -93,7 +41,7 @@ resource "aws_route_table_association" "internet" {
             "sudo apt-get -y upgrade",
             "sudo apt-get -y install sbt curl linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates",
             "curl -s http://yum.dockerproject.org/gpg | sudo apt-key add",
-            "sudo add-apt-repository \"deb https://apt.dockerproject.org/repo/pool/ $(lsb_release -cs) main",
+            "sudo add-apt-repository \"deb https://apt.dockerproject.org/repo/ ubuntu-xenial main\"",
             "sudo apt-get update",
             "sudo apt-get -y install docker-engine"
         ]
@@ -108,4 +56,4 @@ resource "aws_route_table_association" "internet" {
 
 output "connect_ip" {
     value = "${aws_instance.sbt_test.public_ip}"
-}*/
+}
