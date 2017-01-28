8f8c3fa6381e3c582af3fc0bee8b58827d21c049
William Rory Kronmiller <wrk961@gmail.com>
2017-01-27 21:31:26 -0500

    Implement dynamic cluster head connection

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/Vehicle.scala
+++ src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/Vehicle.scala
@@ -1,5 +1,10 @@
 package edu.rpi.cs.nsl.spindle.vehicle.simulation
 
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
 import scala.reflect.runtime.universe
 import scala.reflect.runtime.universe._
 import scala.reflect.runtime.universe.typeTag
@@ -12,29 +17,28 @@ import akka.actor.Props
 import akka.dispatch.BoundedMessageQueueSemantics
 import akka.dispatch.RequiresMessageQueue
 import akka.event.Logging
+import akka.pattern.ask
 import edu.rpi.cs.nsl.spindle.datatypes.{ Vehicle => VehicleMessage }
 import edu.rpi.cs.nsl.spindle.datatypes.VehicleColors
 import edu.rpi.cs.nsl.spindle.datatypes.VehicleTypes
+import scala.concurrent.duration._
 import edu.rpi.cs.nsl.spindle.vehicle.Types._
 import edu.rpi.cs.nsl.spindle.vehicle.kafka.ClientFactory
+import edu.rpi.cs.nsl.spindle.vehicle.kafka.streams.StreamExecutor
+import edu.rpi.cs.nsl.spindle.vehicle.kafka.utils.TopicLookupService
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.CacheTypes
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.CacheTypes
+import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.EventStore
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.Position
+import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.TSCache
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.TSEntryCache
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.sensors.MockSensor
-import edu.rpi.cs.nsl.spindle.vehicle.kafka.utils.TopicLookupService
-import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.TransformationStore
-import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.ActiveTransformations
-import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.MapperFunc
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.KvReducerFunc
+import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.MapperFunc
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.TransformationFunc
-import edu.rpi.cs.nsl.spindle.vehicle.kafka.streams.StreamExecutor
-import java.util.concurrent.Executors
-import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.TSCache
-import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.EventStore
+import edu.rpi.cs.nsl.spindle.vehicle.simulation.transformations.TransformationStore
+import akka.util.Timeout
 
 /**
  * Wraps a value to prevent type erasure
@@ -141,24 +145,38 @@ class Vehicle(nodeId: NodeId,
   }
   private lazy val (timestamps, caches): (Seq[Timestamp], CacheMap) = eventStore.mkCaches(nodeId)
 
-  //TODO: add/integreate VehicleConnection actor
-  private lazy val clusterHeadConnection = VehicleConnection.props(nodeId, clientFactory)
-  //TODO: add cluster membership cache
+  /**
+   * Actor responsible for relaying data to cluster head
+   */
+  private val clusterHeadConnection: ActorRef = {
+    logger.debug(s"Node $nodeId creating cluster head connection actor")
+    val actorRef: ActorRef = context.actorOf(VehicleConnection.props(nodeId, clientFactory.getConfig), name = s"clusterHeadConnector$nodeId")
+    //NOTE: insert monitoring here (once message-based tick is set up)
+    actorRef
+  }
 
   /**
    * Create a Vehicle status message
    */
-  private[simulation] def generateMessage(timestamp: Timestamp): VehicleMessage = {
+  private[simulation] def generateMessage(currentSimTime: Timestamp): VehicleMessage = {
     import VehicleTypes._
     import CacheTypes._
     val readings: Seq[TypedValue[Any]] = ({
-      val position: Position = caches(PositionCache)
+      val positionSeq = caches(PositionCache)
         .asInstanceOf[TSEntryCache[Position]]
-        .getValue(timestamp)
-      val mph = TypedValue[MPH](position.speed)
-      val lat = TypedValue[Lat](position.x)
-      val lon = TypedValue[Lon](position.y)
-      mockSensors.toSeq.map(_.getReading(timestamp)) ++ Seq[TypedValue[_]](mph, lat, lon)
+        .getValueOpt(currentSimTime)
+        .map { position =>
+          val mph = TypedValue[MPH](position.speed)
+          val lat = TypedValue[Lat](position.x)
+          val lon = TypedValue[Lon](position.y)
+          Seq[TypedValue[_]](mph, lat, lon)
+        }
+        .getOrElse {
+          logger.error(s"No position information available for time $currentSimTime in timestamps $timestamps for cache ${caches(PositionCache).cache}")
+          throw new RuntimeException(s"No position information for $currentSimTime")
+        }
+
+      mockSensors.toSeq.map(_.getReading(currentSimTime)) ++ positionSeq
     }).asInstanceOf[Seq[TypedValue[Any]]]
     VehicleMessageFactory.mkVehicle(readings, fullProperties)
   }
@@ -169,14 +187,18 @@ class Vehicle(nodeId: NodeId,
    * Create mapping from simulator time to future epoch times based on startTime
    */
   private[simulation] def mkTimings(startTime: Timestamp): Seq[TimeMapping] = {
+    logger.debug(s"Generating timestamps from start time $startTime using sim times $timestamps")
     val zeroTime = timestamps.min
     val offsets = timestamps.map(_ - zeroTime)
+    assert(offsets.length == timestamps.length)
+    assert(offsets(0) == 0)
     val absoluteTimes = offsets
       .map(_ + startTime)
       .sorted
       .zip(timestamps)
       .sorted
       .map { case (wallTime, simTime) => TimeMapping(wallTime, simTime) }
+    assert(absoluteTimes.head.wallTime == startTime, s"Start time $startTime does not map to ${absoluteTimes.head}")
     // Ensure ascending order
     absoluteTimes
   }
@@ -193,18 +215,29 @@ class Vehicle(nodeId: NodeId,
   }
 
   private trait TemporalDaemon {
-    def executeInterval(currentTiming: Timestamp): Unit
+    def executeInterval(currentSimTime: Timestamp): Unit
   }
 
   private object SensorDaemon extends TemporalDaemon {
     private lazy val outTopic = TopicLookupService.getVehicleStatus(nodeId)
     private val producer = clientFactory.mkProducer[NodeId, VehicleMessage](outTopic)
-    def executeInterval(currentTiming: Timestamp) {
-      val message = generateMessage(currentTiming)
+    def executeInterval(currentSimTime: Timestamp) {
+      val message = generateMessage(currentSimTime)
       producer.send(nodeId, message)
       logger.debug(s"$nodeId sent status message")
     }
   }
+
+  private object ClusterMembershipDaemon extends TemporalDaemon {
+    def executeInterval(currentSimTime: Timestamp) {
+      val clusterHead: NodeId = caches(CacheTypes.ClusterCache).asInstanceOf[TSEntryCache[NodeId]].getOrPriorOpt(currentSimTime).get
+      logger.info(s"Node $nodeId has cluster head $clusterHead at time $currentSimTime")
+      Await.result((clusterHeadConnection ? VehicleConnection.SetClusterHead(clusterHead)), Duration.Inf)
+      logger.info(s"Node $nodeId has updated cluster head $clusterHead at time $currentSimTime")
+    }
+  }
+
+  implicit val connectionActorTimeout = Timeout(10 minutes)//TODO: this should be within 1 second
 
   private object MapReduceDaemon extends TemporalDaemon {
     private lazy val pool = context.dispatcher //TODO: replace with Executors.newCachedThreadPool?
@@ -217,7 +250,7 @@ class Vehicle(nodeId: NodeId,
     }
     private def updateTransformers[T <: TransformationFunc](toRun: Iterable[T], prevMap: Map[T, StreamExecutor]): Map[T, StreamExecutor] = {
       val (toRemove, toAdd) = getChanges(prevMap.keySet, toRun.toSet)
-      toRemove.map(prevMap(_)).foreach(_.stop)
+      toRemove.map(prevMap(_)).foreach(_.stopStream)
       val newTransformers = toAdd.map(func => (func -> func.getTransformExecutor(clientFactory))).toMap
       newTransformers.values.foreach(pool.execute(_))
       newTransformers.values.foreach(transformer => logger.debug(s"Launched executor $transformer"))
@@ -233,20 +266,21 @@ class Vehicle(nodeId: NodeId,
       assert(nextReducers.keySet equals reducers.toSet)
       prevReducers = nextReducers
     }
-    def executeInterval(currentTiming: Timestamp) {
+    def executeInterval(currentSimTime: Timestamp) {
       val ActiveTransformations(mappers, reducers) = transformationStore
-        .getActiveTransformations(currentTiming)
+        .getActiveTransformations(currentSimTime)
       updateMappers(mappers)
       updateReducers(reducers)
+      Await.result((clusterHeadConnection ? VehicleConnection.SetMappers(mappers.map(_.funcId).toSet)), Duration.Inf) //TODO: clean up 
       logger.info(s"$nodeId updated mappers and reducers: $prevMappers $prevReducers")
     }
   }
 
-  private lazy val temporalDaemons = Seq(SensorDaemon, MapReduceDaemon)
+  private lazy val temporalDaemons = Seq(SensorDaemon, MapReduceDaemon, ClusterMembershipDaemon)
 
-  private def executeInterval(currentTiming: Timestamp) {
-    logger.info(s"$nodeId executing for $currentTiming")
-    temporalDaemons.foreach(_.executeInterval(currentTiming))
+  private def executeInterval(currentSimTime: Timestamp) {
+    logger.info(s"$nodeId executing for $currentSimTime: $temporalDaemons")
+    temporalDaemons.foreach(_.executeInterval(currentSimTime))
   }
 
   private def tick(timings: Seq[TimeMapping], replyWhenDone: Option[ActorRef]) {
@@ -276,7 +310,7 @@ class Vehicle(nodeId: NodeId,
     logger.debug(s"$nodeId generated timings ${timings(0)} to ${timings.last}")
     sleepUntil(timings.head.wallTime)
     logger.info(s"Simulation running")
-    tick(timings.tail, replyWhenDone)
+    tick(timings, replyWhenDone)
   }
 
   override def preStart {
@@ -301,6 +335,10 @@ class Vehicle(nodeId: NodeId,
     case Vehicle.CheckReadyMessage => {
       logger.info("Got checkReady")
       sender ! Vehicle.ReadyMessage(nodeId)
+    }
+    case Terminated(clusterHeadConnection) => {
+      logger.error(s"Cluster head connection crashed for $nodeId")
+      throw new RuntimeException(s"$nodeId clusterhead conn crashed")
     }
     case _ => throw new RuntimeException(s"Unknown message on $nodeId")
   }
