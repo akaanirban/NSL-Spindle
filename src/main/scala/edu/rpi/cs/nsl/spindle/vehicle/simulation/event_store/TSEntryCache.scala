c3a8bfd087aaf1dc60e8d51b56e1bcb5d31168c8
William Rory Kronmiller <wrk961@gmail.com>
2017-01-27 21:36:39 -0500

    Allow querying by approximate time and allow optional results

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/TSEntryCache.scala
+++ src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/TSEntryCache.scala
@@ -8,23 +8,41 @@ object CacheTypes extends Enumeration {
   val PositionCache, ClusterCache = Value
 }
 import CacheTypes._
+import scala.concurrent.duration.FiniteDuration
 
 trait TSCache[T] {
-  protected val cache: Map[Timestamp, T]
-  def getValue(timestamp: Timestamp): T = cache.getOrPrior(timestamp)
+  val cache: Map[Timestamp, T] //TODO: protected
+  def getValueOpt(currentSimTime: Timestamp): Option[T] = cache.get(currentSimTime)
+  def getOrPriorOpt(currentSimTime: Timestamp): Option[T] = cache.getOrPriorOpt(currentSimTime)
   def getTimestamps: Iterable[Timestamp] = cache.keys
 
   /**
    * Map from timestamp to value that can return the closest prior value
    */
   protected implicit class TSMap[T](map: Map[Timestamp, T]) {
-    def getOrPrior(timestamp: Timestamp): T = {
+    def getOrPriorOpt(timestamp: Timestamp): Option[T] = {
       val priorTimes = map.keys.filter(_ <= timestamp)
-      assert(priorTimes.isEmpty == false, s"No times for $timestamp in $map")
-      val nearestTime = priorTimes.max
-      map(nearestTime)
+      if (priorTimes.isEmpty) {
+        None
+      } else {
+        val nearestTime = priorTimes.max
+        Some(map(nearestTime))
+      }
     }
   }
+}
+
+/**
+ * Contains database time-series data
+ *
+ * @param timesetamp - database timestamp (in seconds)
+ */
+abstract class TSEntry[T](timestamp: FiniteDuration) {
+  // Get timestamp in milliseconds
+  def getTimestamp: Long = {
+    timestamp.toMillis
+  }
+  def getReading: T
 }
 
 /**
@@ -33,7 +51,7 @@ trait TSCache[T] {
  * @todo - clean this up
  */
 class TSEntryCache[T](readings: Iterable[TSEntry[T]]) extends TSCache[T] {
-  protected val cache: Map[Timestamp, T] = readings
+  val cache: Map[Timestamp, T] = readings
     .map { reading =>
       (reading.getTimestamp -> reading.getReading)
     }
