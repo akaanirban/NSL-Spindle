7d0b1580fffc24b2d2db0e49b5214b8eeafec185
William Rory Kronmiller <wrk961@gmail.com>
2017-01-27 21:35:16 -0500

    Add feature to get a list of nodes that exist at the same time as specified node

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/PgCacheLoader.scala
+++ src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/PgCacheLoader.scala
@@ -6,17 +6,30 @@ import edu.rpi.cs.nsl.spindle.vehicle.Types._
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.postgres.PgClient
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.postgres.PgConfig
 import edu.rpi.cs.nsl.spindle.vehicle.simulation.event_store.postgres.PgDefaults
+import org.slf4j.LoggerFactory
 
 class PgCacheLoader(config: PgConfig = PgDefaults.config) extends PgClient(config) with EventStore {
+  private val logger = LoggerFactory.getLogger(this.getClass)
   private lazy val positionQuery = new PositionQuery(connection)
   private lazy val clusterHeadQuery = new ClusterMembershipQuery(connection, Configuration.Vehicles.clusterMemberTable)
   private lazy val metadataQuery = new MetadataQuery(connection)
+  private lazy val concurrentQuery = new ConcurrentNodesQuery(connection)
   def getNodes: Stream[NodeId] = metadataQuery.loadNodeIds
   def mkCaches(nodeId: NodeId): (Seq[Timestamp], CacheMap) = {
     val positionCache = new TSEntryCache[Position](positionQuery.loadReadings(nodeId))
     val clusterHeadCache = new TSEntryCache[NodeId](clusterHeadQuery.loadClusters(nodeId))
     val cacheMap = Map(PositionCache -> positionCache, ClusterCache -> clusterHeadCache)
-    val timestamps = cacheMap.values.map(_.getTimestamps.toSet).reduce(_ ++ _).toSeq.sorted //TODO: ensure timestamps are sorted in right order
+    val timestamps = cacheMap
+      .values
+      .map(_.getTimestamps.toSet)
+      .reduce(_ ++ _)
+      // Nothing must come before first position reading is available
+      .filterNot(_ < positionCache.getTimestamps.min)
+      .toSeq
+      .sorted
+    assert(timestamps.head < timestamps.last)
     (timestamps, cacheMap)
   }
+  def getConcurrentNodes(nodeId: NodeId): Iterable[NodeId] = concurrentQuery
+    .loadConcurrentNodes(nodeId)
 }
