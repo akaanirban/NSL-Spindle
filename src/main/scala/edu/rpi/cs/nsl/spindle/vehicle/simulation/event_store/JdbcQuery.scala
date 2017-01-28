dfb6b37a00866c814b804fcfca95ecf72408bc4f
William Rory Kronmiller <wrk961@gmail.com>
2017-01-27 21:33:27 -0500

    Allow setNode to use configurable argNum

--- src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/JdbcQuery.scala
+++ src/main/scala/edu/rpi/cs/nsl/spindle/vehicle/simulation/event_store/JdbcQuery.scala
@@ -6,8 +6,8 @@ import java.sql.ResultSet
 
 class JdbcQuery(connection: Connection, statement: String) {
   private val preparedStatement = connection.prepareStatement(statement)
-  protected def setNode(node: NodeId) {
-    preparedStatement.setInt(1, node)
+  protected def setNode(node: NodeId, argNum: Int = 1) {
+    preparedStatement.setInt(argNum, node)
   }
   protected def executeQuery: ResultSet = preparedStatement.executeQuery
 }
