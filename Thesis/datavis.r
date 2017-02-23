3debe72b569e9674ac64f00690e83c05820cdc46
William Rory Kronmiller <wrk961@gmail.com>
2017-02-22 19:05:23 -0500

    Rename title, configuration axis

--- Thesis/datavis.r
+++ Thesis/datavis.r
@@ -14,12 +14,12 @@ getdb <- function() {
 getRuns <- function(dbconn, mrJob) {
     query <- {paste("select * from normed_runs where  mapreducename='", paste(mrJob, "'", sep=""), sep="")}
     print(query);
-    df_results <- transform(dbGetQuery(dbconn, query), uniqueconfig=interaction(clustertable, filtertablename, windowsizems))
+    df_results <- transform(dbGetQuery(dbconn, query), configuration=interaction(clustertable, filtertablename, windowsizems))
     print(df_results);
-    normed_pot <- ggplot(df_results, aes(x=uniqueconfig, y=normed_total_bytes)) + geom_bar(aes(fill = filtertablename), stat="identity") + theme(axis.text.x = element_text(angle=90), plot.title=element_text(hjust = 0.5)) + labs(title = "speedSum Normalized Bytes Transferred to Cloud");
+    normed_pot <- ggplot(df_results, aes(x=configuration, y=normed_total_bytes)) + geom_bar(aes(fill = filtertablename), stat="identity") + theme(axis.text.x = element_text(angle=90), plot.title=element_text(hjust = 0.5)) + labs(title = "speedSum Normalized Bytes Transferred to Cloud");
     normed_plot_path <- paste(mrJob, "runplot-normalized.pdf", sep="-");
     ggsave(file=normed_plot_path)
-    total_plot <- ggplot(df_results, aes(x=uniqueconfig, y=avg_total_bytes)) + geom_bar(aes(fill = filtertablename), stat="identity") + theme(axis.text.x = element_text(angle=90), plot.title=element_text(hjust = 0.5)) + labs(title = "speedSum Normalized Bytes Transferred to Cloud");
+    total_plot <- ggplot(df_results, aes(x=configuration, y=avg_total_bytes)) + geom_bar(aes(fill = filtertablename), stat="identity") + theme(axis.text.x = element_text(angle=90), plot.title=element_text(hjust = 0.5)) + labs(title = "speedSum Total Bytes Transferred to Cloud");
     avg_plot_path <- paste(mrJob, "runplot.pdf", sep="-");
     ggsave(file=avg_plot_path)
 }
