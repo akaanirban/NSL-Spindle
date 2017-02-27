7dd7e887026407e00cc203fde279ec926217866c
William Rory Kronmiller <wrk961@gmail.com>
2017-02-27 10:11:02 -0500

    Add figures for system sequence and component diagrams

--- Thesis/index.js
+++ Thesis/index.js
@@ -3,22 +3,35 @@ const exec = require('child_process').exec;
 
 const filePath = 'rpithes-short.tex';
 const pdfPath = 'rpithes-short.pdf';
+const binDir = './binimages';
 
 watch.createMonitor('./', (monitor) => {
     monitor.files[filePath];
     monitor.on('changed', (f, stat) => {
         if(f.endsWith('.mmd')){
-            exec(`mermaid --width 1920 ${f}`, (err) => {
+            exec(`mermaid --width 1920 --outputDir ${binDir} ${f}`, (err) => {
                 if(err) { throw err; }
                 console.log('Compiled mermaid', f);
+                const [, fileNameWithExt] = f.split('/');
+                const [fileName, ] = fileNameWithExt.split('.');
+                exec(`mv ${binDir}/${fileNameWithExt}.png ${binDir}/${fileName}.png`, (err) => {
+                    if(err) { throw err; }
+                    console.log('moved mermaid');
+                });
             });
             return;
         } else if(f !== filePath) {
+            console.log('Skipping file', f);
             return;
         }
-        exec(`pdflatex ${filePath}`, (err, stdout, stderr) => {
+        exec(`pdflatex -halt-on-error ${filePath}`, (err, stdout, stderr) => {
             if(err) { throw err; }
-            exec(`open ${pdfPath}`);
+            console.log('Compiled tex. Opening file');
+            exec(`open ${pdfPath}`, (err, stdout, stderr) => {
+                if(err) { throw err;}
+                console.log(stdout);
+                console.error(stderr);
+            });
         });
     });
 });
