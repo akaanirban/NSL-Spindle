a26ac92e5804e3d2ee263e2c6a111cb1f3284755
William Rory Kronmiller <wrk961@gmail.com>
2017-03-01 12:03:47 -0500

    Run compiler in background

---
+++ Thesis/compiler.js
@@ -0,0 +1,46 @@
+const watch = require('watch');
+const exec = require('child_process').exec;
+
+const filePath = 'rpithes-short.tex';
+const pdfPath = 'rpithes-short.pdf';
+const binDir = './binimages';
+const datavisR = 'datavis.r';
+
+watch.createMonitor('./', (monitor) => {
+    monitor.files[filePath];
+    monitor.on('changed', (f, stat) => {
+        if(f.endsWith('.mmd')){
+            exec(`mermaid --width 1920 --outputDir ${binDir} ${f}`, (err) => {
+                if(err) { throw err; }
+                console.log('Compiled mermaid', f);
+                const [, fileNameWithExt] = f.split('/');
+                const [fileName, ] = fileNameWithExt.split('.');
+                exec(`mv ${binDir}/${fileNameWithExt}.png ${binDir}/${fileName}.png`, (err) => {
+                    if(err) { throw err; }
+                    console.log('moved mermaid');
+                });
+            });
+            return;
+        } else if(f === datavisR) {
+          return exec(`Rscript ${f}`, (err, stdout, stderr) => {
+            if(err) {
+              return console.error(stdout, stderr);
+            }
+          });  
+        } else if(f !== filePath) {
+            console.log('Skipping file', f);
+            return;
+        }
+        exec(`pdflatex -halt-on-error ${filePath}`, (err, stdout, stderr) => {
+            if(err) { 
+                return console.error(stdout, stderr);
+            }
+            console.log('Compiled tex. Opening file');
+            exec(`open ${pdfPath}`, (err, stdout, stderr) => {
+                if(err) { throw err;}
+                console.log(stdout);
+                console.error(stderr);
+            });
+        });
+    });
+});
--- Thesis/index.js
+++ Thesis/index.js
@@ -1,46 +1,13 @@
-const watch = require('watch');
-const exec = require('child_process').exec;
+const fs = require('fs');
+const spawn = require('child_process').spawn;
 
-const filePath = 'rpithes-short.tex';
-const pdfPath = 'rpithes-short.pdf';
-const binDir = './binimages';
-const datavisR = 'datavis.r';
+const logFile = fs.openSync('compiler.log', 'w');
 
-watch.createMonitor('./', (monitor) => {
-    monitor.files[filePath];
-    monitor.on('changed', (f, stat) => {
-        if(f.endsWith('.mmd')){
-            exec(`mermaid --width 1920 --outputDir ${binDir} ${f}`, (err) => {
-                if(err) { throw err; }
-                console.log('Compiled mermaid', f);
-                const [, fileNameWithExt] = f.split('/');
-                const [fileName, ] = fileNameWithExt.split('.');
-                exec(`mv ${binDir}/${fileNameWithExt}.png ${binDir}/${fileName}.png`, (err) => {
-                    if(err) { throw err; }
-                    console.log('moved mermaid');
-                });
-            });
-            return;
-        } else if(f === datavisR) {
-          return exec(`Rscript ${f}`, (err, stdout, stderr) => {
-            if(err) {
-              return console.error(stdout, stderr);
-            }
-          });  
-        } else if(f !== filePath) {
-            console.log('Skipping file', f);
-            return;
-        }
-        exec(`pdflatex -halt-on-error ${filePath}`, (err, stdout, stderr) => {
-            if(err) { 
-                return console.error(stdout, stderr);
-            }
-            console.log('Compiled tex. Opening file');
-            exec(`open ${pdfPath}`, (err, stdout, stderr) => {
-                if(err) { throw err;}
-                console.log(stdout);
-                console.error(stderr);
-            });
-        });
-    });
+const proc = spawn('node', ['compiler.js'], {
+    stdio: ['ignore', logFile, logFile],
+    detached: true
 });
+
+fs.writeFileSync('compiler.pid', `${proc.pid}`);
+
+proc.unref();
