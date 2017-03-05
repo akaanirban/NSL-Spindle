f50d8aa7de3be6489ebecde8f75c2d412060e5d4
William Rory Kronmiller <wrk961@gmail.com>
2017-03-05 10:11:45 -0500

    Remove old results display app

--- showResults/buildDocker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t wkronmiller/spindle-results:latest .
-docker push wkronmiller/spindle-results:latest
--- showResults/launchPM2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pm2 start --name spindle-show-results lib/index.js --interpreter ./node_modules/.bin/babel-node
