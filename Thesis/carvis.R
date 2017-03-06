8c62c63bcba5575a91696220eec4672b33614fda
William Rory Kronmiller <wrk961@gmail.com>
2017-03-05 19:38:17 -0500

    Update figures and descriptions

--- Thesis/carvis.R
+++ Thesis/carvis.R
@@ -7,14 +7,15 @@ source("database.R");
 # TODO: use outlines for boxen
 main <- function() {
     dbconn <- getdb();
-    query <- { "select x,y, speed from node_positions where timestamp=21903" }
+    query <- { "select x,y from node_positions where timestamp=21903" }
     df_results <- dbGetQuery(dbconn, query)
     print(df_results)
     p <- ggplot(df_results, aes(x, y)) + 
         theme_bw() +
         geom_rect(aes(xmin=47150, xmax=53000, ymin=106801, ymax=112545), color='darkblue', linetype=2, alpha=0) +
         geom_rect(aes(xmin=50000, xmax=51000, ymin=100000, ymax=102000), color='darkred', linetype=1, alpha=0) +
-        geom_point(aes(colour=speed), size=0.5) + scale_color_gradient(low='darkred', high='green', trans='log')
+        geom_point(size=0.5) +
+        scale_fill_grey();
     ggsave(file="binImages/cars.png")
 }
 
--- Thesis/datavis.r
+++ Thesis/datavis.r
@@ -16,24 +16,33 @@ getRuns <- function(dbconn, mrJob) {
     query <- {paste("select * from normed_runs where clustertable !='self_clusters' and mapreducename='", paste(mrJob, "'", sep=""), sep="")}
     print(query);
     df_results <- transform(dbGetQuery(dbconn, query), configuration=replaceText(interaction(clustertable, filtertablename, paste(sapply(windowsizems,function(x) x / 1000), "s"), sep="\n")))
-    print(df_results);
     normed_pot <- ggplot(df_results, aes(x=configuration, y=normed_total_bytes)) + 
+        theme_bw() +
         geom_bar(aes(fill = filtertablename), stat="identity") + 
         scale_y_continuous(name="Normalized Bytes to Middleware", limits=c(0,1)) +
         theme(text=element_text(size=15), axis.text.x=element_text(angle=60, vjust=0.5),plot.title=element_text(hjust = 0.5)) + 
-        labs(title = paste(mrJob, "Normalized Bytes Transferred to Cloud"));
+        labs(title = paste(mrJob, "Normalized Bytes Transferred to Cloud")) +
+        scale_fill_grey()
     normed_plot_path <- paste("binImages/", paste(mrJob, "runplot-normalized.pdf", sep="-"), sep="");
     ggsave(file=normed_plot_path)
     query <- {paste("select * from normed_runs where mapreducename='", paste(mrJob, "'", sep=""), sep="")}
-    print(query);
     df_results <- transform(dbGetQuery(dbconn, query), configuration=replaceText(interaction(clustertable, filtertablename, paste(sapply(windowsizems,function(x) x / 1000), "s"), sep="\n")))
-    print(df_results);
     total_plot <- ggplot(df_results, aes(x=configuration, y=avg_total_bytes)) + 
+        theme_bw() +
         geom_bar(aes(fill = filtertablename), stat="identity") + 
         theme(text=element_text(size=15), axis.text.x = element_text(angle=60, vjust=0.5), plot.title=element_text(hjust = 0.5)) + 
-        labs(title = paste(mrJob, "Total Bytes Transferred to Cloud"));
+        labs(title = paste(mrJob, "Total Bytes Transferred to Cloud")) + 
+        scale_fill_grey()
     avg_plot_path <- paste("binImages/", paste(mrJob, "runplot.pdf", sep="-"), sep="");
     ggsave(file=avg_plot_path)
+}
+
+compareAll <- function(dbconn) {
+    query <- {
+        "select * from normed_runs";
+    }
+    df_results <- dbGetQuery(dbconn, query);
+    print(df_results);
 }
 
 main <- function() {
@@ -41,6 +50,7 @@ main <- function() {
     getRuns(dbconn, "speedSum");
     getRuns(dbconn, "geoMapped");
     getRuns(dbconn, "geoFiltered");
+    compareAll(dbconn);
 }
 
 main();
