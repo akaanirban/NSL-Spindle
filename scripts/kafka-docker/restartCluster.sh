57b56a97e92f4f20b867ad038936ca875285db90
Anirban <anirban.dash@gmail.com>
2017-04-18 18:58:25 -0400

    Add compatibility with Ubuntu ifconfig

--- scripts/kafka-docker/restartCluster.sh
+++ scripts/kafka-docker/restartCluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export HOSTNAME=`ifconfig | grep -oE "\binet ([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep -v "127.0.0.1" | grep -oE "\b([0-9]){1,3}.*\b"`
+export HOSTNAME=`ifconfig | grep -oE "\binet (addr:){0,1}([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep -v "127.0.0.1" | grep -oE "\b([0-9]){1,3}.*\b" | grep -v "172.18" | grep -v "172.17" | grep -v "172.16"`
+echo "HOSTNAME $HOSTNAME"
 if [[ -z "$HOSTNAME" ]]; then
     echo "Failed to get inet IP"
 fi
