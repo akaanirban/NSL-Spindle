09d7a2ae85a2268a5698a807a2e5d2db33d1410d
root <root@nero.kronmiller.net>
2017-04-26 18:11:11 -0400

    Add raspi emulator files

--- Vehicle/Vehicle-Node/docker/raspi/emuLikeAQemu.sh
+++ Vehicle/Vehicle-Node/docker/raspi/emuLikeAQemu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 qemu-system-arm -no-reboot \
-  -kernel ./qemu-rpi-kernel/kernel-qemu-3.10.25-wheezy \
+  -kernel ./qemu-rpi-kernel/kernel-qemu-4.4.13-jessie \
   -cpu arm1176 \
   -m 256 -M versatilepb \
   -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-  -clock dynticks \
-  -hda 2015-02-16-raspbian-wheezy.img \
+  -k en-us \
+  -hda 2017-04-10-raspbian-jessie-lite.img \
   -redir tcp:2222::22
