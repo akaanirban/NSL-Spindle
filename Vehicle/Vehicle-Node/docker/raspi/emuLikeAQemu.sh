9348e2aa12f67c93ce5c9f69d8fc79627a04d994
William Rory Kronmiller <wrk961@gmail.com>
2017-04-26 18:51:04 -0400

    Delete raspi emulator directory (not working)

--- Vehicle/Vehicle-Node/docker/raspi/emuLikeAQemu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-qemu-system-arm -no-reboot \
-  -kernel ./qemu-rpi-kernel/kernel-qemu-4.4.13-jessie \
-  -cpu arm1176 \
-  -m 256 -M versatilepb \
-  -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-  -k en-us \
-  -hda 2017-04-10-raspbian-jessie-lite.img \
-  -redir tcp:2222::22
--- Vehicle/Vehicle-Node/docker/raspi/mountThaThingy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p mount
-LOOPDEV=`losetup -f --show -P 2017-04-10-raspbian-jessie-lite.img`
-P2=p2
-FULLDEV=$LOOPDEV$P2
-echo $FULLDEV
-sleep 1
-mount $FULLDEV ./mount
