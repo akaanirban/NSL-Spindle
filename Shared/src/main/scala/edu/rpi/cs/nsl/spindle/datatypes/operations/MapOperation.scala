f6198195b1f1f05920691fc48e37bf39e9ca5146
William Rory Kronmiller <wrk961@gmail.com>
2017-05-04 11:50:02 -0400

    Add filter operation

--- Shared/src/main/scala/edu/rpi/cs/nsl/spindle/datatypes/operations/MapOperation.scala
+++ Shared/src/main/scala/edu/rpi/cs/nsl/spindle/datatypes/operations/MapOperation.scala
@@ -3,5 +3,5 @@ package edu.rpi.cs.nsl.spindle.datatypes.operations
 import scala.reflect.runtime.universe._
 
 //TODO: explicit key and value types
-case class MapOperation[InType: TypeTag, OutType: TypeTag](override val uid: String = java.util.UUID.randomUUID.toString, f: (InType) => OutType)
+case class MapOperation[InType: TypeTag, OutType: TypeTag](f: (InType) => OutType, override val uid: String = java.util.UUID.randomUUID.toString, filter: (InType) => Boolean = (_: InType) => true)
   extends Operation[InType, OutType](uid, OperationIds.map)
---
+++ Test-Spark-Program/src/main/scala/edu/rpi/cs/nsl/spindle/spark/test/Main.scala
@@ -0,0 +1,10 @@
+package edu.rpi.cs.nsl.spindle.spark.test
+
+/**
+  * Created by wrkronmiller on 5/3/17.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf
+  }
+}
