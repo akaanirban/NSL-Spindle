d1bce0753228573ec2706131b59e1b10b243a8b6
William Rory Kronmiller <wrk961@gmail.com>
2017-05-16 16:58:43 -0400

    Refactor mapper output topics and relay control flow

--- Test-Spark-Program/src/main/scala/edu/rpi/cs/nsl/spindle/spark/test/Main.scala
+++ Test-Spark-Program/src/main/scala/edu/rpi/cs/nsl/spindle/spark/test/Main.scala
@@ -3,7 +3,7 @@ package edu.rpi.cs.nsl.spindle.spark.test
 import edu.rpi.cs.nsl.spindle.QueryUidGenerator
 import edu.rpi.cs.nsl.v2v.spark.streaming.NSLUtils
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
 
 
 class MockQueryUidGenerator extends QueryUidGenerator {
@@ -15,9 +15,9 @@ class MockQueryUidGenerator extends QueryUidGenerator {
   */
 object Main {
   def main(args: Array[String]): Unit = {
-    val TOPIC = "spindle-vehicle-reducer-sumSpeedAndCount"
+    val TOPIC = "spindle-vehicle-reducer-outputs"
     val sc = new SparkConf().setAppName("SparkSpindleTest").setMaster("local[*]")
-    val ssc = new StreamingContext(sc, Seconds(60))
+    val ssc = new StreamingContext(sc, Minutes(1))
     val stream = NSLUtils.createVStream(ssc, NSLUtils.StreamConfig("localhost:2182", "localhost:9093", TOPIC), new MockQueryUidGenerator)
       .map(v => (null, (v.mph, 1.toLong)))
       .reduceByKey{case (a,b) => (a._1 + b._1, a._2 + b._2)}
