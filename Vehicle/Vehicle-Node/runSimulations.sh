9c3d219b5c9f5142350a30b04a9934eae76f759d
connor foody <connorfoody@gmail.com>
2018-04-20 15:16:05 -0400

    seem to have multiple clusters running

--- Vehicle/Vehicle-Node/runSimulations.sh
+++ Vehicle/Vehicle-Node/runSimulations.sh
@@ -6,12 +6,12 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-#close if there are any stray containers
-if [[ `docker ps|grep "SPINDLE"|awk '{print $1}'` ]]
-then  
-   echo "Killing old Spindle processes"
-   docker kill $(docker ps|grep "SPINDLE"|awk '{print $1}')
-fi
+##close if there are any stray containers
+#if [[ `docker ps|grep "SPINDLE"|awk '{print $1}'` ]]
+#then
+#   echo "Killing old Spindle processes"
+#   docker kill $(docker ps|grep "SPINDLE"|awk '{print $1}')
+#fi
 
 # Write code to start up Cluster
 echo
@@ -23,18 +23,17 @@ echo
 # NOTE: modify this variable when running the scrip
 # you can find this address by running ifconfig
 #####################################################
-MIDDLEWARE_IP=192.168.133.160
+MIDDLEWARE_IP=192.168.133.161
 numberOfNodes=$1
-
+adjust=$2
 #ports are published to bind with the host for the cluster head
 docker run -it --rm -d \
 		-e MIDDLEWARE_HOSTNAME=$MIDDLEWARE_IP\
-    -e NODE_ID=0\
-    -e NUM_NODES=$numberOfNodes\
-		-p 9001:9001 -p 2182:2182 -p 9093:9093\
-		--name SPINDLE-CLUSTERHEAD\
+        -e NODE_ID=0\
+        -e NUM_NODES=$numberOfNodes\
+		--name SPINDLE-CLUSTERHEAD$adjust\
 		nslrpi/spindle-node
-ClusterHeadIP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' SPINDLE-CLUSTERHEAD`
+ClusterHeadIP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' SPINDLE-CLUSTERHEAD$adjust`
 
 
 #Set the middleware hostname, cluster head broker and zookeeper config for nodes
@@ -61,6 +60,6 @@ do
         -e NUM_NODES=$numberOfNodes\
 				-e CLUSTERHEAD_ZK_STRING=$ClusterHeadIP:2182\
         -e NODE_ID=$i\
-				--name $nodeName$i\
+				--name $nodeName$i$adjust\
 				nslrpi/spindle-node
 done
