c6c29a40b9bcc3db27cd800ada60337faa406b76
connor foody <connorfoody@gmail.com>
2018-05-07 14:22:32 -0400

    updated spark run code

--- Test-Spark-Program/runSpark.sh
+++ Test-Spark-Program/runSpark.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# runs the spark, automagically finds the ip address of the kafka container we need
-MIDDLEWARE_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' MIDDLEWARE-KAFKA)
-echo "middleware is:"
-echo ${MIDDLEWARE_IP}
-sbt run MIDDLEWARE_IP
+# runs spark in the docker container
+if [[ "$(docker images -q nslrpi/test-spark:latest 2> /dev/null)" == "" ]]; then
+  docker pull nslrpi/test-spark
+fi
+
+# assumes that the spindle bridge is all set up
+docker run --rm -it --net=SPINDLE-BRIDGE --name testSpark nslrpi/test-spark:latest
